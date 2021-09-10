710ba19dd7d3ff74529957da8ca5204719d5be74
Guillermo Velasco <alfadoble10@gmail.com>
2021-09-10 20:49:07 -0300

    first commit - incluye todo creacion,busqueda, listado y delete

---
+++ index.js
@@ -0,0 +1,84 @@
+const { response } = require('express')
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+
+let persons= [
+    {
+      "name": "b",
+      "number": "3",
+      "id": 2
+    },
+    {
+      "name": "c",
+      "number": "5",
+      "id": 3
+    },
+    {
+      "name": "e",
+      "number": "1",
+      "id": 4
+    }
+]
+
+app.get('/info',(req,res)=>{
+    res.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
+app.get('/api/persons',(req,res)=>{
+    res.status(200).json(persons)
+})
+
+app.get('/api/persons/:id',(req,res)=>{
+    const id= Number(req.params.id)
+    const person=persons.find(pers=>pers.id===id)
+    if(person){
+        res.status(200).json(person)
+    }
+    else res.status(404).end()
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+    const id= Number(req.params.id)
+    persons=persons.filter(pers=>pers.id!==id)
+    res.status(204).end()
+})
+
+const generateId=()=>{
+    return Math.random()*1000
+}
+
+const getFindName=(name)=>{
+    return persons.find(pers=>pers.name===name)
+}
+
+app.post('/api/persons',(req,res)=>{
+    const {name,number}= req.body
+
+    if(!name){
+        res.status(404).json({'error':'name is required'})
+    }
+
+    if(getFindName(name)){
+        res.status(404).json({'error':'name must be unique'})
+    }
+    
+    if(!number){
+        res.status(404).json({'error':'number is required'})
+    }
+
+    const person={
+        name,
+        number,
+        id:generateId()
+    }
+
+    persons=persons.concat(person)
+    res.json(person)
+})
+
+PORT=3001
+app.listen(PORT,()=>{
+    console.log(`Servidor Corriendo en Puerto: ${PORT}`)
+})
